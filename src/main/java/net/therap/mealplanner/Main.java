9002cb2b9485a59b73641f7ab11d20b34eddcc9e
Pranjal Chakraborty <pranjal.chakraborty@therapservices.net>
2017-05-09 12:18:22 +0600

    prints meal plan

--- src/main/java/net/therap/mealplanner/Main.java
+++ src/main/java/net/therap/mealplanner/Main.java
@@ -7,11 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+import java.util.Scanner;
+
 /**
  * @author pranjal.chakraborty
  * @since 5/8/17
  */
 public class Main {
+
+    private static final String COMMA = ", ";
+
     public static void main (String [] args) throws Exception{
         Class.forName("com.mysql.jdbc.Driver");
         System.out.println("Driver found");
@@ -20,12 +25,43 @@ public class Main {
                         + "user=pranjal&password=password");
         System.out.println("Connected");
 
-        System.out.println("______Day______");
-        writeResultSetDay(connect.createStatement().executeQuery("select * from mealplanner.day"));
+        System.out.println("______Meal planner______");
+        System.out.println("1: see the current meal plan");
+        System.out.println("2: create your own");
+        int inputFromUser = Integer.parseInt(new Scanner(System.in).nextLine());
 
-        System.out.println("______Items______");
-        writeResultSetItems(connect.createStatement().executeQuery("select * from mealplanner.items"));
-        connect.close();
+        switch(inputFromUser){
+            case 1: writeResultSetMenu(connect.createStatement().executeQuery(
+                    "select a.dayName, d.itemName, e.itemName, f.itemName, g.itemName, h.itemName, i.itemName " +
+                    "from day a,breakfast b, lunch l, meal c, items d, items e, items f, items g, items h, items i " +
+                    "where a.countDay=c.dayId and b.item1 = d.id and b.item2 = e.id and b.item3 = f.id and" +
+                    " l.item1 = g.id and l.item2 = h.id and l.item3 = i.id"));
+                break;
+            case 2: writeResultSetItems(connect.createStatement().executeQuery("select * from mealplanner.items"));
+                break;
+            default: connect.close();
+
+        }
+    }
+
+    private static void writeResultSetMenu(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+
+        while (resultSet.next()) {
+            String day = resultSet.getString(1);
+
+            String breakfastItem1 = resultSet.getString(2);
+            String breakfastItem2 = resultSet.getString(3);
+            String breakfastItem3 = resultSet.getString(4);
+
+            String lunchItem1 = resultSet.getString(5);
+            String lunchItem2 = resultSet.getString(6);
+            String lunchItem3 = resultSet.getString(7);
+
+            System.out.println("Day: "+ day);
+            System.out.println("Breakfast: "+ breakfastItem1 + COMMA + breakfastItem2 + COMMA + breakfastItem3);
+            System.out.println("Lunch: "+ lunchItem1 + COMMA + lunchItem2 + COMMA + lunchItem3);
+        }
     }
 
     private static void writeResultSetDay(ResultSet resultSet) throws SQLException {
@@ -51,4 +87,6 @@ public class Main {
             System.out.println(itemName);
         }
     }
+
+
 }
