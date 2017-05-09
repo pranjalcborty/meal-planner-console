1da1a1485be3e01b020ea087da8eac9086c78a9d
Pranjal Chakraborty <pranjal.chakraborty@therapservices.net>
2017-05-09 16:09:32 +0600

    before making pretty

--- src/main/java/net/therap/mealplanner/Main.java
+++ src/main/java/net/therap/mealplanner/Main.java
@@ -13,29 +13,35 @@ import java.util.Scanner;
 public class Main {
 
     private static final String COMMA = ", ";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_AUTHENTICATION = "jdbc:mysql://localhost/mealplanner?"
+            + "user=pranjal&password=password";
+    private static final String CONNECTION_ESTABLISHED = "Connected";
+    private static final String COUNTDAY_COLUMN = "countDay";
+    private static final String DAYNAME_COLUMN = "dayName";
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Driver found");
+        Class.forName(DRIVER);
+
         Connection connect = DriverManager
-                .getConnection("jdbc:mysql://localhost/mealplanner?"
-                        + "user=pranjal&password=password");
-        System.out.println("Connected");
+                .getConnection(DATABASE_AUTHENTICATION);
+
+        System.out.println(CONNECTION_ESTABLISHED);
 
         while(true) {
-
+            System.out.println("************************");
             System.out.println("______Meal planner______");
             System.out.println("1: see the current meal plan");
             System.out.println("2: create your own");
+
             int inputFromUser = Integer.parseInt(new Scanner(System.in).nextLine());
 
             switch (inputFromUser) {
                 case 1:
-                    writeResultSetMenu(connect.createStatement().executeQuery(
-                            "select a.dayName, d.itemName, e.itemName, f.itemName, g.itemName, h.itemName, i.itemName " +
-                                    "from day a,breakfast b, lunch l, meal c, items d, items e, items f, items g, items h, items i " +
-                                    "where a.countDay=c.dayId and b.item1 = d.id and b.item2 = e.id and b.item3 = f.id and" +
-                                    " l.item1 = g.id and l.item2 = h.id and l.item3 = i.id and c.breakfastid = b.id and c.lunchid = l.id"));
+                    writeResultSetMenu(connect, connect.createStatement().executeQuery(
+                            "select d.dayName, b.item1, b.item2, b.item3, l.item1, l.item2, l.item3 " +
+                                    "from day d, meal m, breakfast b, lunch l " +
+                                    "where m.dayid = d.countDay and m.breakfastid = b.id and m.lunchid = l.id"));
                     break;
                 case 2:
                     customPlan(connect);
@@ -48,19 +54,53 @@ public class Main {
         }
     }
 
-    private static void writeResultSetMenu(ResultSet resultSet) throws SQLException {
-        // ResultSet is initially before the first data set
+    private static void writeResultSetMenu(Connection connect, ResultSet resultSet) throws SQLException {
 
         while (resultSet.next()) {
             String day = resultSet.getString(1);
 
-            String breakfastItem1 = resultSet.getString(2);
-            String breakfastItem2 = resultSet.getString(3);
-            String breakfastItem3 = resultSet.getString(4);
+            String breakfastItem1 = "";
+            String breakfastItem2 = "";
+            String breakfastItem3 = "";
+            String lunchItem1 = "";
+            String lunchItem2 = "";
+            String lunchItem3 = "";
 
-            String lunchItem1 = resultSet.getString(5);
-            String lunchItem2 = resultSet.getString(6);
-            String lunchItem3 = resultSet.getString(7);
+            ResultSet resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(2)));
+            if(resultSet1.next()) {
+                breakfastItem1 = resultSet1.getString(1);
+            }
+
+            resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(3)));
+            if(resultSet1.next()) {
+                breakfastItem2 = resultSet1.getString(1);
+            }
+
+            resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(4)));
+            if(resultSet1.next()) {
+                breakfastItem3 = resultSet1.getString(1);
+            }
+
+            resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(5)));
+            if(resultSet1.next()) {
+                lunchItem1 = resultSet1.getString(1);
+            }
+
+            resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(6)));
+            if(resultSet1.next()) {
+                lunchItem2 = resultSet1.getString(1);
+            }
+
+            resultSet1 = (connect.createStatement().executeQuery("select itemName from items where id =" +
+                    resultSet.getString(7)));
+            if(resultSet1.next()) {
+                lunchItem3 = resultSet1.getString(1);
+            }
 
             System.out.println("Day: " + day);
             System.out.println("Breakfast: " + breakfastItem1 + COMMA + breakfastItem2 + COMMA + breakfastItem3);
@@ -70,8 +110,8 @@ public class Main {
 
     private static void writeResultSetDay(ResultSet resultSet) throws SQLException {
         while (resultSet.next()) {
-            int countDay = resultSet.getInt("countDay");
-            String dayName = resultSet.getString("dayName");
+            int countDay = resultSet.getInt(COUNTDAY_COLUMN);
+            String dayName = resultSet.getString(DAYNAME_COLUMN);
             System.out.print(countDay + "\t");
             System.out.println(dayName);
         }
@@ -94,7 +134,8 @@ public class Main {
             String item3 = resultSet.getString("item3");
             System.out.print(itemId + "\t");
 
-            ResultSet resultSet1 = connect.createStatement().executeQuery("select itemName from items where id=" + item1 + " or id=" + item2 + " or id=" + item3);
+            ResultSet resultSet1 = connect.createStatement().executeQuery("select itemName from items where id=" +
+                    item1 + " or id=" + item2 + " or id=" + item3);
 
             while (resultSet1.next()){
                 System.out.print(resultSet1.getString(1)+", ");
@@ -108,7 +149,8 @@ public class Main {
         writeResultSetDay(connect.createStatement().executeQuery("select * from mealplanner.day"));
         int dayId = Integer.parseInt(new Scanner(System.in).nextLine());
 
-        ResultSet resultSet = connect.createStatement().executeQuery("select dayName from mealplanner.day where countDay=" + dayId);
+        ResultSet resultSet = connect.createStatement().executeQuery("select dayName from mealplanner.day " +
+                "where countDay=" + dayId);
 
         if (resultSet.next()) {
             System.out.println("The day you chose: " + resultSet.getString("dayName"));
@@ -123,7 +165,8 @@ public class Main {
                 case 1:
                     String slotName = "breakfast";
 
-                    ResultSet resultSet1 = connect.createStatement().executeQuery("select * from mealplanner.breakfast");
+                    ResultSet resultSet1 = connect.createStatement().executeQuery(
+                            "select * from mealplanner.breakfast");
 
                     if (resultSet1.next()) {
                         System.out.println("You can choose from existing sets...");
@@ -133,13 +176,16 @@ public class Main {
 
                         if (opinion.equals("y")) {
                             System.out.println("Enter the id of the set");
-                            ResultSet resultSet2 = connect.createStatement().executeQuery("select * from meal where dayid=" + dayId);
+                            ResultSet resultSet2 = connect.createStatement().executeQuery(
+                                    "select * from meal where dayid=" + dayId);
                             int id = Integer.parseInt(new Scanner(System.in).nextLine());
 
                             if (resultSet2.next()) {
-                                connect.createStatement().executeUpdate("update meal set breakfastid = " + id + " where dayid=" + dayId);
+                                connect.createStatement().executeUpdate("update meal set breakfastid = " +
+                                        id + " where dayid=" + dayId);
                             } else {
-                                connect.createStatement().executeUpdate("insert into meal(id, dayid, breakfastid) values(default, " + dayId + ", " + id + ")");
+                                connect.createStatement().executeUpdate("insert into meal(id, dayid, breakfastid) " +
+                                        "values(default, " + dayId + ", " + id + ")");
                             }
 
                             flag = true;
@@ -152,13 +198,23 @@ public class Main {
                         String[] itemIds = new Scanner(System.in).nextLine().split(" ");
 
                         connect.createStatement().executeUpdate("insert into " + slotName +
-                                "(id, item1, item2, item3) " + "values " + "(default, " + itemIds[0] + ", " + itemIds[1] + ", " + itemIds[2] + ")");
+                                "(id, item1, item2, item3) " + "values " + "(default, " + itemIds[0] + ", " +
+                                itemIds[1] + ", " + itemIds[2] + ")");
 
+                        ResultSet resultSetCheck = connect.createStatement().executeQuery("select * from meal " +
+                                "where dayid=" + dayId);
                         ResultSet resultSet2 = connect.createStatement().executeQuery("select max(id) from breakfast");
                         if(resultSet2.next()) {
                             int id = resultSet2.getInt(1);
-                            connect.createStatement().executeUpdate("insert into meal(id, dayid, breakfastid) values(default, " + dayId + ", " + id + ")");
-                            System.out.println("Success");
+
+                            if(resultSetCheck.next()) {
+                                connect.createStatement().executeUpdate("update meal set breakfastid = " +
+                                        id + " where dayid=" + dayId);
+                            }
+                            else {
+                                connect.createStatement().executeUpdate("insert into meal(id, dayid, breakfastid) " +
+                                        "values(default, " + dayId + ", " + id + ")");
+                            }
                         }
                     }
 
@@ -175,13 +231,16 @@ public class Main {
 
                         if (opinion.equals("y")) {
                             System.out.println("Enter the id of the set");
-                            ResultSet resultSet2 = connect.createStatement().executeQuery("select * from meal where dayid=" + dayId);
+                            ResultSet resultSet2 = connect.createStatement().executeQuery("select * from meal " +
+                                    "where dayid=" + dayId);
                             int id = Integer.parseInt(new Scanner(System.in).nextLine());
 
                             if (resultSet2.next()) {
-                                connect.createStatement().executeUpdate("update meal set lunchid = " + id + " where dayid=" + dayId);
+                                connect.createStatement().executeUpdate("update meal set lunchid = " + id +
+                                        " where dayid=" + dayId);
                             } else {
-                                connect.createStatement().executeUpdate("insert into meal(id, dayid, lunchid) values(default, " + dayId + ", " + id + ")");
+                                connect.createStatement().executeUpdate("insert into meal(id, dayid, lunchid) " +
+                                        "values(default, " + dayId + ", " + id + ")");
                             }
 
                             flag = true;
@@ -194,12 +253,26 @@ public class Main {
                         String[] itemIds = new Scanner(System.in).nextLine().split(" ");
 
                         connect.createStatement().executeUpdate("insert into " + slotName +
-                                "(id, item1, item2, item3) " + "values " + "(default, " + itemIds[0] + ", " + itemIds[1] + ", " + itemIds[2] + ")");
+                                "(id, item1, item2, item3) " + "values " + "(default, " + itemIds[0] + ", " +
+                                itemIds[1] + ", " + itemIds[2] + ")");
 
+
+
+                        ResultSet resultSetCheck = connect.createStatement().executeQuery("select * from meal " +
+                                "where dayid=" + dayId);
                         ResultSet resultSet2 = connect.createStatement().executeQuery("select max(id) from lunch");
+
                         if(resultSet2.next()) {
                             int id = resultSet2.getInt(1);
-                            connect.createStatement().executeUpdate("insert into meal(id, dayid, lunchid) values(default, " + dayId + ", " + id + ")");
+
+                            if (resultSetCheck.next()) {
+                                connect.createStatement().executeUpdate("update meal set lunchid = " + id +
+                                        " where dayid=" + dayId);
+                            }
+                            else {
+                                connect.createStatement().executeUpdate("insert into meal(id, dayid, lunchid) " +
+                                        "values(default, " + dayId + ", " + id + ")");
+                            }
                         }
                     }
 
